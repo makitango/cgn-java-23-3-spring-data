1a658cb12f787be729db6b358ab6b95acd541d3d
Marcell <marcell.dechant@neuefische.de>
2023-10-17 10:02:07 +0200

    Add CarServiceTest, enhance test coverage
    The commit includes a new test file CarServiceTest.java in the car package of the application. It consists of three unit tests to verify the functionality of the methods addCar(), getAllCars(), and getCarById() in the CarService class. With the help of Mockito, we simulate the interactions between the service class and repository.
    
    The car package previously lacked sufficient test coverage, which risked unidentified bugs and defects. The introduction of these tests is aimed at improving the reliability of the car-related functions and ensuring they behave as expected under different circumstances.

---
+++ src/test/java/de/neuefische/cgnjava233springdata/car/CarServiceTest.java
@@ -0,0 +1,56 @@
+package de.neuefische.cgnjava233springdata.car;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class CarServiceTest {
+
+	private CarRepository carRepository = mock(CarRepository.class);
+	private CarService carService = new CarService(carRepository);
+
+	@Test
+	void addCar() {
+		// GIVEN
+		NewCar newCarToSave = new NewCar("test", "test");
+		Car expected = new Car("1", "test", "test");
+		// WHEN
+		when(carRepository.save(any(Car.class))).thenReturn(expected);
+		Car actual = carService.addCar(newCarToSave);
+		// THEN
+		verify(carRepository).save(any(Car.class));
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void getAllCars() {
+		// GIVEN
+		List<Car> expected = List.of(
+				new Car("1", "test", "test"),
+				new Car("2", "test", "test")
+		);
+		// WHEN
+		when(carRepository.findAll()).thenReturn(expected);
+		List<Car> actual = carService.getAllCars();
+		// THEN
+		verify(carRepository).findAll();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void getCarById() {
+		// GIVEN
+		Car expected = new Car("1", "test", "test");
+		// WHEN
+		when(carRepository.findById(expected.id())).thenReturn(Optional.of(expected));
+		Car actual = carService.getCarById(expected.id());
+		// THEN
+		verify(carRepository).findById(expected.id());
+		assertEquals(expected, actual);
+	}
+}
